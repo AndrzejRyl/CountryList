1ab8ae166835fd3ac4cbe934f65da524928ac6bc
Andrzej Ryl <andrzej.ryl@gmail.com>
2020-06-18 14:57:28 +0200

    Add default activity and main fragment with navigation to it

---
+++ app/src/main/kotlin/com/ryl/countrylist/presentation/countrylist/CountryListFragment.kt
@@ -0,0 +1,6 @@
+package com.ryl.countrylist.presentation.countrylist
+
+import androidx.fragment.app.Fragment
+import com.ryl.countrylist.R
+
+class CountryListFragment  : Fragment(R.layout.fragment_country_list)
---
+++ app/src/main/kotlin/com/ryl/countrylist/presentation/main/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.ryl.countrylist.presentation.main
+
+import androidx.appcompat.app.AppCompatActivity
+import com.ryl.countrylist.R
+
+
+class MainActivity : AppCompatActivity(R.layout.activity_main)
